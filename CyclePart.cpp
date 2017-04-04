74bc4966d35d1c2d8f8cd49abdff4bd6709c9308
MDonchev <mariodonchew@gmail.com>
2017-04-04 17:20:02 +0300

    Simple Bicycle

--- CyclePart.cpp
+++ CyclePart.cpp
@@ -2,12 +2,11 @@
 #include<cstring>
 
 using namespace std;
-
 enum Quality
 {
     Low =1, Medium , High
-};
 
+};
 class CyclePart
 {
     char* name;
@@ -87,7 +86,10 @@ public:
     {
         return quality;
     }
-
+    bool operator==(CyclePart& other)
+    {
+        return strcmp(name,other.getName()) == 0 && price == other.getPrice() && quality == other.getQuality();
+    }
 //---------------------------------------------------------------------------------------
 
     void print()
@@ -98,65 +100,14 @@ public:
 
 
 
-class Becycle{
+class Becycle
+{
 
-char* brand;
-CyclePart* parts;
-int _size;
-int _count;
-double _price;
-
-
-public:
-    Becycle()
-    {
-        setBrand("");
-        _size=1;
-        _count=0;
-        _price=0;
-        parts=new CyclePart[_size];
-    }
-    Becycle(char* _brand)
-    {
-        setBrand(_brand);
-        _size=1;
-        _count=0;
-        _price=0;
-        parts=new CyclePart[_size];
-    }
-    Becycle(const Becycle& other)
-    {
-        setBrand(other.brand);
-        _size=other._size;
-        _count=other._count;
-        _price=other._price;
-        for(int i=0;i<other._count;i++)
-            addPart(other.parts[i]);
-    }
-    Becycle& operator=(const Becycle& other)
-    {
-        if(this==&other)
-        {
-        setBrand(other.brand);
-        _size=other._size;
-        _count=other._count;
-        _price=other._price;
-        for(int i=0;i<other._count;i++)
-            addPart(other.parts[i]);
-        }
-        return *this;
-    }
-    ~Becycle()
-    {
-        delete brand;
-        delete parts;
-    }
-
-
-
-//---------------------------------------------------------------------------------------
-
-
+    char* brand;
+    CyclePart* parts;
+    int _size;
+    int _count;
+    double _price;
     void addPart(CyclePart part)
     {
         if(_count==_size)
@@ -173,7 +124,82 @@ public:
         _count++;
         _price+=part.getPrice();
     }
+    void remPart(CyclePart& part)
+    {
+        for (int i=0; i<_count; i++)
+        {
+            if (parts[i] == part)
+            {
+                for (int j = i+1; j<_count; j++)
+                {
+                    parts[j-1] = parts[j];
+                }
+                _count--;
+                _price -= parts[i].getPrice();
+            }
+        }
+    }
+    int countHighQuality()
+    {
+        int counter = 0;
+        for (int i=0;i<_count;i++)
+        {
+            if(parts[i].getQuality() == Quality::High)
+                counter++;
+        }
+    }
+public:
+    Becycle(): brand(NULL)
+    {
+        setBrand("");
+        _size=1;
+        _count=0;
+        _price=0;
+        parts=new CyclePart[_size];
+    }
+    Becycle(char* _brand): brand(NULL)
+    {
+        setBrand(_brand);
+        _size=1;
+        _count=0;
+        _price=0;
+        parts=new CyclePart[_size];
+    }
+    Becycle(const Becycle& other): brand(NULL), parts(NULL)
+    {
+        setBrand(other.brand);
+        _size=other._size;
+        _count=other._count;
+        _price=other._price;
+        delete[] parts;
+        parts = new CyclePart [_size];
+        for(int i=0;i<_count;i++)
+            parts[i] = other.parts[i];
+    }
+    Becycle& operator=(const Becycle& other)
+    {
+        if(this==&other)
+        {
+        setBrand(other.brand);
+        _size=other._size;
+        _count=other._count;
+        _price=other._price;
+        delete[] parts;
+        parts = new CyclePart [_size];
+        for(int i=0;i<_count;i++)
+            parts[i] = other.parts[i];
+        }
+        return *this;
+    }
+    ~Becycle()
+    {
+        delete brand;
+        delete parts;
+    }
 
+
+
+//--------------------------------------------------------------------------------------
 
     void print()
     {
@@ -187,8 +213,51 @@ public:
         return _price*1.5;
     }
 
-
-
+    Becycle& operator+(const CyclePart& part)
+    {
+        addPart(part);
+        return *this;
+    }
+    Becycle& operator-(CyclePart& part)
+    {
+        remPart(part);
+        return *this;
+    }
+    //is the brand same??
+    bool operator==(const Becycle& other)
+    {
+        return strcmp(brand,other.brand)==0;
+    }
+    //is the price not equal
+    bool operator!=(Becycle& other)
+    {
+        return !(getPrice() == other.getPrice());
+    }
+    bool operator<(Becycle& other)
+    {
+        return countHighQuality() < other.countHighQuality();
+    }
+    bool operator<=(Becycle& other)
+    {
+        return countHighQuality() <= other.countHighQuality();
+    }
+    bool operator>(Becycle& other)
+    {
+        return countHighQuality() > other.countHighQuality();
+    }
+    bool operator>=(Becycle& other)
+    {
+        return countHighQuality() >= other.countHighQuality();
+    }
+    CyclePart* operator[](const char* name)
+    {
+        for (int i=0;i<_count;i++)
+        {
+            if( strcmp(name,parts[i].getName()) == 0)
+                return parts + i;
+        }
+        return NULL;
+    }
 //---------------------------------------------------------------------------------------
     void setBrand(char* _brand)
     {
@@ -206,11 +275,32 @@ int main()
     CyclePart three("seat",5,Quality::Low);
 
     Becycle bmx("bmx");
+    Becycle bmx1("bmx");
+    bmx1+one;
+    bmx1+two;
+    bmx1+two;
+    bmx1+two;
+    bmx1+two;
 
-    bmx.addPart(one);
-    bmx.addPart(two);
-    bmx.addPart(three);
+    bmx+one;
+    bmx+two;
+    bmx+three;
+    bmx+one;
+    bmx+two;
+    bmx+one;
     bmx.print();
-
+    bmx - one;
+    bmx.print();
+    bmx+two;
+    bmx+two;
+    bmx.print();
+    std::cout<<(bmx == bmx1)<<std::endl;
+    std::cout<<(bmx != bmx1)<<std::endl;
+    std::cout<<(bmx <= bmx1)<<std::endl;
+    const char* fName = "tier"; // test for operator[]
+    if (bmx[fName])
+        bmx[fName]->print() ;
+    else
+        std::cout<<"No such part"<<std::endl;
     return 0;
 }
